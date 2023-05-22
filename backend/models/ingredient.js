2a2938af631bd19c51343b65bdc12eef64c5d036
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-05-21 21:33:18 -0700

    Adding ci workflows

--- backend/models/ingredient.js
+++ backend/models/ingredient.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
 
 const IngredientSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        imageLink: {
-            type: String,
-            required: false,
-            trim: true,
-        },
-        // don't know if we need the following
-        recipes: [{
-            type: mongoose.Schema.Types.ObjectId, // array of recipe ids (references)
-            ref: "Recipe",
-            required: false,
-            trim: true,
-        }],
-        // ex types: "vegetable", "fruit", "meat", "dairy", "grain", "spice", "condiment", "other"
-        type: {
-            type: String,
-            required: false,
-            trim: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    { collection: "ingredients" }
+    imageLink: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    // don't know if we need the following
+    recipes: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // array of recipe ids (references)
+        ref: "Recipe",
+        required: false,
+        trim: true,
+      },
+    ],
+    // ex types: "vegetable", "fruit", "meat", "dairy", "grain", "spice", "condiment", "other"
+    type: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+  },
+  { collection: "ingredients" }
 );
 
 const Ingredient = mongoose.model("Ingredient", IngredientSchema);
