81324853ad654b454ca7637f690613efb119b44b
Westluu <west1luu@gmail.com>
2023-06-08 17:59:05 -0700

    CD testing

--- react-frontend/src/components/context/AuthProvider.js
+++ react-frontend/src/components/context/AuthProvider.js
@@ -47,6 +47,7 @@ export const AuthProvider = ({ children }) => {
     console.log("In Login");
 
     try{
+      console.log("BACKEND URL: ", `${process.env.REACT_APP_BACKEND_URL}/login`);
       console.log(`${process.env.REACT_APP_BACKEND_URL}/login`);
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, user);
       if (response.status === 201){
