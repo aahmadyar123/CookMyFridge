47269fba7e4a8aafe0924d7e390c47dbc8a03474
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-06-09 17:27:12 -0700

    Added hardcoded value

--- react-frontend/src/components/context/dotenv.js
+++ react-frontend/src/components/context/dotenv.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  BACKEND_URL: process.env.REACT_APP_BACKEND_URL
+  //BACKEND_URL: process.env.REACT_APP_BACKEND_URL
+  BACKEND_URL: "https://cookmyfridge-backend.azurewebsites.net"
   // Add other environment variables here
 };
